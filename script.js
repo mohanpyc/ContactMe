8cb591b56ae5bba8f224431a5da7a2e35e819173
mohanpyc <mohanpyc@gmail.com>
2020-11-12 23:12:30 +0530

    used setTimeout to remove temporary classNames

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let btn=document.querySelector("#btn");
 
 btn.addEventListener("click",()=>{
-    let infoDivs=document.querySelectorAll(".contactInfo");
-    btn.classList.add("btnRounded");
-    btn.textContent="^";
-    infoDivs.forEach((i,index)=>{
-        setTimeout( ()=>{i.classList.add("displayInfo")},index*350)
-        console.log(i);
-    })
+    console.log(btn.value);
+    if(btn.value!="Click Meee"){
+        btn.value="Click Meee";
+        btn.className="btnTablet";
+        let infoDivs=document.querySelectorAll(".contactInfo");
+        infoDivs.forEach((i,index)=>{
+            setTimeout( ()=>{i.className="contactInfo"},index*350)
+        })
+
+    }else{
+        let infoDivs=document.querySelectorAll(".contactInfo");
+        btn.classList.add("btnRounded");
+        btn.value="^";
+        infoDivs.forEach((i,index)=>{
+            setTimeout( ()=>{i.classList.add("displayInfo")},index*350)
+            console.log(i);
+        })
+    }
+
+
+
+    
 
 })
