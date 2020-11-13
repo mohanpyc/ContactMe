363effc9e9ac6c4e2092a625452d01deb148c8fd
mohanpyc <mohanpyc@gmail.com>
2020-11-13 11:16:34 +0530

    added transition effects to contactMe

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let btn=document.querySelector("#btn");
 
 btn.addEventListener("click",()=>{
-    console.log(btn.value);
     if(btn.value!="Click Meee"){
+        let contact=document.querySelector('.contact');
+        contact.classList.remove("contactBorder");
+        let contactMe=document.querySelector(".contactMe");
+        contactMe.classList.remove("contactMeTrans")
         btn.value="Click Meee";
         btn.className="btnTablet";
         let infoDivs=document.querySelectorAll(".contactInfo");
@@ -11,6 +14,10 @@ btn.addEventListener("click",()=>{
         })
 
     }else{
+        let contact=document.querySelector('.contact');
+        contact.classList.add("contactBorder");
+        let contactMe=document.querySelector(".contactMe");
+        contactMe.classList.add("contactMeTrans")
         let infoDivs=document.querySelectorAll(".contactInfo");
         btn.classList.add("btnRounded");
         btn.value="^";
